b620e6fcc788323934027e3619c4af3329d83570
Arti13x <artivoid13@gmail.com>
2026-09-02 18:48:30 -0500

    initial commit

---
+++ Source/GAM312SurvivalGame/GAM312SurvivalGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312SurvivalGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312SurvivalGame, "GAM312SurvivalGame" );
